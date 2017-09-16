70759cacf5471a3f74c47cc21e15340c15926018
miki <miodragmilosevic84miki@gmail.com>
2017-09-17 00:27:59 +0200

    Update about page copy

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -12,10 +12,8 @@ var About = (props) => {
   return (
     <div>
 
-    <h3>About Component!</h3>
-    <p>
-Welcome to the about page.
-    </p>
+    <h3>About</h3>
+    <p>Welcome to the about page.</p>
     </div>
   )
 };
