2a9029f0abbebe50cf24e13564d94f99e4c46c64
miki <miodragmilosevic84miki@gmail.com>
2017-09-16 23:45:03 +0200

    Prep repo for Heroku

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(express.static("public"));
+//app.use(express.static("public"));
 
 app.use(function (req, res, next){
   if (req.headers['x-forwarded-proto'] === 'http') {
@@ -12,9 +12,10 @@ app.use(function (req, res, next){
     else.redirect('http://' + req.hostname + req.url);
     }
 });
-
+app.use(express.static('public'));
 console.log("Hello!");
 
-app.listen(PORT,() => {
+//app.listen(PORT,() => {
+app.listen(PORT, function () {
   console.log("server starter port 3000" + PORT);
 });
